0fccbff1538ab1a96c3cd0f3135d7b0ee9d05411
nanami <otamao74@yahoo.co.jp>
2015-03-02 22:06:58 +0900

    issue #1 課題2提出_2

--- okt73/02_total.rb
+++ okt73/02_total.rb
@@ -1,20 +1,10 @@
 # encoding:Shift_JIS
 values =[4,9,5,1,3,8,6,2,7,10]
 i = 0.0
-max = 0
-min = 11
 std =0.0
 
 values.each do |n|
-	i += n.to_i #合計値
-	
-	if n > max
-		max = n #最大
-	end
-	
-	if n < min
-		min = n #最小
-	end
+	i += n #合計値
 end
 
 values.each do |n|
@@ -22,4 +12,4 @@ values.each do |n|
 end
 std = Math.sqrt(std/values.size) #標準偏差
 
-puts "合計値:#{i.to_i}","最大値:#{max}","最小値:#{min}","相加平均:#{i/values.size}","標準偏差:#{std}"
+puts "合計値:#{i.to_i}","最大値:#{values.max}","最小値:#{values.min}","相加平均:#{i/values.size}","標準偏差:#{std}"
