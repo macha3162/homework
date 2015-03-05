9cea47709126ee63920e9263f90ee491245c07f0
masuda <masuda.shigeki@synergy101.jp>
2015-03-05 16:57:22 +0900

    issue #4 ページタイトルのプログラム改善

--- okt73/get_title.rb
+++ okt73/get_title.rb
@@ -6,6 +6,6 @@ url = ARGV[0].to_s
 
 open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE) do |io|
 	str = io.read
-	str =~ /<title>\n*(.+)\n*<\/title>/i
-	puts $1
+	str =~ /<title>(.+)<\/title>/im
+	puts $1.strip
 end
