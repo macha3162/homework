458c19f9e8a95a0ea6cc1cacaaf847b72af05e12
nanami <otamao74@yahoo.co.jp>
2015-03-07 17:41:52 +0900

    issue #7 URL抽出課題提出

--- okt73/url_pick.rb
+++ okt73/url_pick.rb
@@ -1,8 +1,7 @@
 # encoding:utf-8
-
 Dir.chdir("..")
 str = File.open("resource/07_index.html").read
 
-(str).scan(/<a\shref=\"(http.*)\"\s?.*?>(.*)<\/a>/) do |matched|
+(str).scan(/<a href=\"(http.*?)\".*>(.*?)<\/a>/) do |matched|
 	puts matched
 end
