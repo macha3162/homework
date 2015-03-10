64c83ee9e2554cdb3444d603a4c01ca390b8b2d7
yoshioka <yoshioka@M.local>
2015-03-10 14:22:31 +0900

    add post code

--- yoshio8927/foo.rb
+++ yoshio8927/foo.rb
@@ -1,14 +1,14 @@
-
 def foo(ary)
-h = Hash.new(0)
+  h = Hash.new(0)
 
 
-  ary.each do |x|  
+  ary.each do |x|
     domain = x.split("@").last
-     h.key?(domain)
-     h[domain] += 1
+    h.key?(domain)
+    h[domain] += 1
   end
   print h
 end
+
 mails=%w(taro@sample.jp nancy@sample.com jiro@sample.jp hermes@synm.jp saburo@sample.jp) #ここにメールアドレスを入れる
 foo(mails)
