112ffb7a999c63b9eeffab4968b3e260bf926670
yoshioka <yossy8927@gmail.com>
2015-03-11 16:09:03 +0900

    show

--- post_code/app/controllers/results_controller.rb
+++ post_code/app/controllers/results_controller.rb
@@ -24,12 +24,9 @@ class ResultsController < ApplicationController
     pp params
     pp '@@@@@@@@@@@@@@@@@@@@@@@'
 
-    if test = params[:code]
-    else
-      test = " "
-    end
-     test = params[:code] || " "
-    @results = Adress.where(["code like ?", "#{test}%"]).all
+
+
+    @result = Adress.find(params[:id])
   end
 
 
