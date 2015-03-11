e205fd01a3bc7806eafad4d6048106faf15021a1
yoshioka <yossy8927@gmail.com>
2015-03-11 14:58:37 +0900

    elsif

--- post_code/app/controllers/results_controller.rb
+++ post_code/app/controllers/results_controller.rb
@@ -1,17 +1,23 @@
 require 'pp'
 
 class ResultsController < ApplicationController
+
+
+
   def index
     pp '@@@@@@@@@@@@@@@@@@@@@@@'
     pp params
     pp '@@@@@@@@@@@@@@@@@@@@@@@'
 
     if test = params[:code]
-    else
-      test = " "
-    end
-    @results = Adress.where("code like '%" + test + "%'").all
+    @results = Adress.where(["code like ?", "#{test}%"]).all
+    elsif test = params[:prefecture]
+    @results = Adress.where(["prefecture like ?", "#{test}%"]).all
+    elsif test.present?
+      end
   end
+
+
 
   def show
     pp '@@@@@@@@@@@@@@@@@@@@@@@'
@@ -25,4 +31,6 @@ class ResultsController < ApplicationController
      test = params[:code] || " "
     @results = Adress.where(["code like ?", "#{test}%"]).all
   end
+
+
 end
